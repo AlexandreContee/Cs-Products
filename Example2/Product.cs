59fa2952ea52724f4ddb910fb39fdce9ac16834d
Alexandre Conte <alexandre.conte10@gmail.com>
2022-11-28 12:22:58 -0300

    Final version exercise

---
+++ Example2/Product.cs
@@ -0,0 +1,8 @@
+﻿namespace Example2
+{
+    internal class Product
+    {
+        public string Name { get; set; }
+        public double Price { get; set; }
+    }
+}
---
+++ Example2/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Globalization;
+
+namespace Example2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Digite a quantidade de produtos a serem cadastrados: ");
+            int N = int.Parse(Console.ReadLine());
+
+            Product[] products = new Product[N];
+
+            for(int i = 0; i < N; i++)
+            {
+                string name = Console.ReadLine();
+                double price = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+                products[i] = new Product { Name = name, Price = price };
+            }
+
+            double sum = 0;
+            for(int i = 0; i < N; i++)
+            {
+                sum += products[i].Price;
+            }
+            
+            double avg = sum / N;
+            Console.WriteLine("The average price is: " + avg.ToString("F2", CultureInfo.InvariantCulture));
+        }
+    }
+}
